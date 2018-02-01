ba9919f5f119ea2eb2cbbc86342e33bc3a0b925f
Arjan de Ruijter <rra.am1a.2017@gmail.com>
2018-02-01 16:22:35 +0100

    Initial commit

---
+++ connect_db.php
@@ -0,0 +1,11 @@
+<?php
+// **********************login gegevens van localhost***************************
+// We gaan contact maken in de code met de mysql database
+define("SERVER_NAME", "localhost");
+define("USER_NAME", "am1a_loginregis");
+define("PASSWORD", "Geheim!");
+define("DATABASE_NAME", "am1a_loginregistration");
+// Met deze functie maken we contact met de mysql server
+$conn = mysqli_connect(SERVER_NAME, USER_NAME, PASSWORD, DATABASE_NAME);
+// *****************************************************************************
+?>
