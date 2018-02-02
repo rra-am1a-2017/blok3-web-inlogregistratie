2bf3949ec4c2c39cf21ed6a1176be2b01e623a6a
Arjan de Ruijter <rra.am1a.2017@gmail.com>
2018-02-02 09:59:06 +0100

    check if exists

--- register.php
+++ register.php
@@ -1,5 +1,41 @@
 <?php
-  var_dump($_POST);
+  // var_dump($_POST);
+  // We gaan contact maken in de code met de mysql database
+  include("./connect_db.php");
+  include("./functions/functions.php");  
+  $email       = sanitize($_POST["email"]);
+  $conditions  = sanitize($_POST["conditions"]);
+
+  // We gaan checken in de database of het ingevoerde emailadres al bestaat in de database
+  $sql = "SELECT * FROM `login` WHERE `email` = '" . $email . "'";
+
+  $result = mysqli_query($conn, $sql);
+
+  if ( mysqli_num_rows($result)) {
+    // Hier moet een redirect komen naar de index.html pagina
+  } 
+  else {
+    // Dit is de query die de ingevulde gegevens kan wegschrijven naar de tabel users
+      $sql = "INSERT INTO `login` (`id`,
+                                   `email`,
+                                   `conditions`,
+                                   `userrole`)
+                          VALUES  (NULL, 
+                                  '" . $email ."', 
+                                  '" . $conditions . "',
+                                  'subscriber');";
+
+      // echo $sql; exit();
+      mysqli_query($conn, $sql);
+      header("Location: ./index.html");
+  }
+  
+
+  
 
 
+
+
+  
+  
 ?>
