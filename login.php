b0be5d7a402a142a409489cfe6b7830bad78f534
Arjan de Ruijter <rra.am1a.2017@gmail.com>
2018-03-09 09:09:05 +0100

    passverify

--- login.php
+++ login.php
@@ -2,6 +2,35 @@
   // Kijk of de velden zijn ingevuld.
   if (!empty($_POST["email"]) && !empty($_POST["password"])) {
 
+    // Maak de sanitize functie beschikbaar
+    include("./functions/functions.php");
+
+    // Maak contact met de mysql-server
+    include("./connect_db.php");
+
+    $email = sanitize($_POST["email"]);
+    $password = sanitize($_POST["password"]);
+
+    $sql = "SELECT * FROM `login` WHERE `email` = '{$email}'";
+
+    $result = mysqli_query($conn, $sql);
+
+
+
+    if ( mysqli_num_rows($result)) {
+
+      $record = mysqli_fetch_array($result);
+
+      if (password_verify($password, $record["password"])) {
+        //ga verder met inlogprocedure
+      } else {
+        header("Location: ./index.php?action=loginform&status=accessdenied");        
+      }
+
+    } else {
+      header("Location: ./index.php?action=loginform&status=accessdenied");
+    }
+
   } else {
     header("Location: ./index.php?action=loginform&status=emptyfields");
   }
